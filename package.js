f43fe8484846c6efb9e134a5ec59bdd45d7028c7
Jan Küster <jkuester@uni-bremen.de>
2020-09-04 14:42:32 +0200

    published 1.0.0

--- package.js
+++ package.js
@@ -1,24 +1,21 @@
+/* eslint-env meteor */
 Package.describe({
   name: 'jkuester:template-monitor',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Monitoring for Blaze templates',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@github.com:jankapunkt/meteor-template-monitor.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
-});
+})
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.11');
-  api.use('ecmascript');
-  api.mainModule('template-monitor.js');
-});
-
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('jkuester:template-monitor');
-  api.mainModule('template-monitor-tests.js');
-});
+Package.onUse(function (api) {
+  api.versionsFrom('1.6')
+  api.use([
+    'ecmascript',
+    'templating@1.3.2'
+  ], 'client')
+  api.addFiles('template-monitor.js', 'client')
+})
